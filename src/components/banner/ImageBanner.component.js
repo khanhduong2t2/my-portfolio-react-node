57a4c7ac3a812ff5c33dacff8b300cc8976456f6
khanhduong2t2 <khanhduong2t2@gmail.com>
2023-07-30 17:28:47 +0700

    [Duong] Update banner

--- src/components/banner/ImageBanner.component.js
+++ src/components/banner/ImageBanner.component.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { imgBanner } from '../../Redux/actions/appActions';
+import React from 'react';
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { imgBanner } from '../../Redux/actions/appActions';
 
 function ImageBanner() {
-    const imagesBanner = useSelector((state) => state.imagesBanner);
-    const { loading, error, results } = imagesBanner;
+    // const imagesBanner = useSelector((state) => state.imagesBanner);
+    // const { loading, error, results } = imagesBanner;
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(imgBanner("img_banner"));
-    }, [dispatch]);
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     dispatch(imgBanner("img_banner"));
+    // }, [dispatch]);
 
     return (
         <div className="screenImage container-2">
-            {
+            {/* {
                 loading ? (
                     <div>
                         <h1>...</h1>
@@ -24,11 +25,11 @@ function ImageBanner() {
                         results.data.data.map((item) => (
                             <img key={item.name} className={item.name} src={item.url} alt="image_banner" />
                         ))
-            }
+            } */}
 
-            {/* <img className="banner_1" src="https://firebasestorage.googleapis.com/v0/b/save-portfolio.appspot.com/o/portfolio%2Fimage-banner-3.jpg?alt=media&token=b0c3b005-6878-420a-a070-c415402370fb" alt="irene blog" />
+            <img className="banner_1" src="https://firebasestorage.googleapis.com/v0/b/save-portfolio.appspot.com/o/portfolio%2Fimage-banner-3.jpg?alt=media&token=b0c3b005-6878-420a-a070-c415402370fb" alt="irene blog" />
             <img className="banner_2" src="https://firebasestorage.googleapis.com/v0/b/save-portfolio.appspot.com/o/portfolio%2Fimage-banner-2.jpg?alt=media&token=7569e138-51b5-43d8-aa0b-3b21af4c070a" alt="irene blog" />
-            <img className="banner_3" src="https://firebasestorage.googleapis.com/v0/b/save-portfolio.appspot.com/o/portfolio%2Fbanner_1.jpg?alt=media&token=26bbf340-ca9f-448c-bc07-98e8f3374103" alt="irene blog" /> */}
+            <img className="banner_3" src="https://firebasestorage.googleapis.com/v0/b/save-portfolio.appspot.com/o/portfolio%2Fbanner_1.jpg?alt=media&token=26bbf340-ca9f-448c-bc07-98e8f3374103" alt="irene blog" />
         </div>
     )
 }
